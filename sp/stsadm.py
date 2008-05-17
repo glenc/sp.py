a6c421ac80ac12bcb0e7956041639456b0576ac3
Glen Cooper <glen@glenc.net>
2008-05-17 13:16:28 -0700

    minor change to stsadm

--- sp/stsadm.py
+++ sp/stsadm.py
@@ -8,4 +8,4 @@ from System.IO import Path
 
 # Get Path to stsadm.exe
 common_dir = Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFiles)
-STSADMPATH = Path.Combine(common_dir, "microsoft shared\\web server extensions\\12\\bin\\stsadm.exe")
+STSADMPATH = Path.Combine(common_dir, "Microsoft Shared\\Web Server Extensions\\12\\Bin\\stsadm.exe")
