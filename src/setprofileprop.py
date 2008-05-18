81bf4b5d47c67c311d0e9d98e00e0f33edf9865c
Glen Cooper <glen@glenc.net>
2008-05-18 16:04:50 -0700

    Updated script to use named params

--- src/setprofileprop.py
+++ src/setprofileprop.py
@@ -1,11 +1,60 @@
-# This script will set a given property to the same value
-# or pattern on all user profiles
+# setprofileprop.py
+"""
+Set Profile Property
+This script will set a property on all user profiles based on the pattern provided.
+
+Usage:
+
+	ipy setprofileprop.py --url http://myserver --prop Picture 
+	                      --value http://myserver/pics/{Alias}.jpg
+
+Arguments:
+
+	url      - server url
+	prop     - property to set
+	value    - value to set the property to
+	[--help] - show this help
+
+"""
 
 import sp
 import re
 from sp import sharedservices
 
 __all__ = ["set_profile_prop"]
+
+def main(argv):
+	from scriptutil import getopt
+	try:
+		opts, args = getopt.getopt(argv, "u:p:v:?", ["url=", "prop=", "value=", "help"])
+	except getopt.GetoptError:
+		showhelp()
+	
+	# defaults
+	url = ""
+	prop = ""
+	value = ""
+	
+	for o,a in opts:
+		if o in ("-u", "--url"):
+			url = a
+		elif o in ("-p", "--prop"):
+			prop = a
+		elif o in ("-v", "--value"):
+			value = a
+		elif o in ("-?", "--help"):
+			showhelp()
+	
+	if url == "" or prop == "":
+		showhelp()
+	
+	set_profile_prop(url, prop, value)
+
+
+def showhelp():
+	print __doc__
+	sys.exit()
+
 
 def set_profile_prop(url, prop, value):
 	"""Executes the script"""
@@ -46,26 +95,4 @@ def _apply_value(profile, prop, value):
 
 if __name__ == '__main__':
 	import sys
-	
-	if len(sys.argv) == 1:
-		print HELPSTRING
-	else:
-		set_profile_prop(sys.argv[1], sys.argv[2], sys.argv[3])
-
-
-
-HELPSTRING = """
-Set Profile Property
-This script will set a property on all user profiles based on the pattern provided.
-
-Usage:
-
-	ipy setprofileprop.py http://myserver Picture http://myserver/pics/{Alias}.jpg
-
-Arguments:
-
-	url      - server url
-	property - property to set
-	value    - value to set the property to
-
-"""
+	main(sys.argv[1:])
