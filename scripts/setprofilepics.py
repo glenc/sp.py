f09035b350918df1b375d5cef63b4faa9d7dcbec
Glen Cooper <glen@glenc.net>
2008-05-17 20:40:51 -0700

    Finished script to set profile props

--- scripts/setprofilepics.py
+++ scripts/setprofilepics.py
@@ -1,31 +1,42 @@
-# This script will set everyone's profile picture
-# based on a given pattern
+# This script will set a given property to the same value
+# or pattern on all user profiles
 
 import sp
 import re
 from sp import sharedservices
 
-def main(url, pattern):
+def main(url, prop, value):
 	"""Executes the script"""
 	
-	# walk over all profiles and call apply_pattern
-	sharedservices.enum_profiles(url, lambda p: apply_pattern(p, pattern))
+	# walk over all profiles and call apply_value
+	sharedservices.enum_profiles(url, lambda p: apply_value(p, prop, value))
 
 
-def apply_pattern(profile, pattern):
-	"""Applies the pattern to the profile provided"""
+def apply_value(profile, prop, value):
+	"""Applies the value to the profile provided"""
 	
-	# take the {pattern}, locate any {key} patterns, then
+	# take the value, locate any {key} patterns, then
 	# replace each with the property from the profile
-	
 	regex = re.compile("{(.*?)}")
-	for match in regex.finditer(pattern):
+	for match in regex.finditer(value):
 		key = match.group(1)
-		val = profile[key].Value
-		pattern = pattern.replace(match.group(0), val)
 		
-	print(pattern)
-	
+		# handle special case where 'Alias' maps to 'AccountName'
+		trim = False
+		if key == "Alias":
+			key = "AccountName"
+			trim = True
+		
+		val = profile[key].Value
+		
+		if trim == True:
+			val = val.split("\\")[1]	
+		
+		value = value.replace(match.group(0), val)
+		
+	# how apply updated value to the profile property
+	profile[prop].Value = value
+	profile.Commit()
 
 
 
@@ -35,19 +46,22 @@ if __name__ == '__main__':
 	if len(sys.argv) == 1:
 		print HELPSTRING
 	else:
-		main(sys.argv[1], sys.argv[2])
+		main(sys.argv[1], sys.argv[2], sys.argv[3])
 
 
-HELPSTRING = """Set Profile Pics
-This script will set all user's profile picture based on the pattern provided.
+
+HELPSTRING = """
+Set Profile Property
+This script will set a property on all user profiles based on the pattern provided.
 
 Usage:
 
-	ipy setprofilepics.py http://myserver http://myserver/pics/{account_name}.jpg
+	ipy setprofilepics.py http://myserver Picture http://myserver/pics/{Alias}.jpg
 
 Arguments:
 
-	url     - server url
-	pattern - pattern to use when setting the picture profile property
+	url      - server url
+	property - property to set
+	value    - value to set the property to
 
 """
